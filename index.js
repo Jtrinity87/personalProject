700367639d73f664544c469dce3c49a42fb8febc
Jtrinity87 <mercado.jacqueline.87@gmail.com>
2020-06-10 21:23:55 -0400

    Additional Changes

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require('express');
 const body_parser = require('body-parser');
 const mongodb = require('mongodb');
 const app = express();
-const PORT = 3000;
+const PORT = 3003;
 const path = require('path');
+const dotenv = require('dotenv').config();
+
+let mongoClient = mongodb.MongoClient;
+
 
 app.set('view engine', 'ejs');
 
@@ -15,12 +19,22 @@ app.use(body_parser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 let db_handler;
-const DB_URL = 'mongodb://localhost:27017';
-const DB_NAME = 'community'
-const COLLECTION_NAME = 'art';
+const DB_URL = process.env.DB_URL;
+const DB_NAME = process.env.DB_NAME;
+const COLLECTION_NAME = process.env.COLLECTION_NAME;
 
 app.listen(PORT, () => {
-    console.log('Server started on 3000');
+    console.log('Server started on 3003');
+    mongoClient.connect(DB_URL, (err, db) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('connected to database');
+            // open database
+            db_handler = db.db(DB_NAME);
+        }
+    });
+
 });
 
 //homepage should be rendered 
@@ -30,26 +44,112 @@ app.get('/', (req, res) => {
 });
 // app.get('/', (req,res) => { res.render('index')}
 
-app.get('/index.ejs', (req, res)=>{
+app.get('/index', (req, res) => {
     res.render('pages/index');
 });
 
-app.get('/aboutPage.ejs', (req, res)=>{
+app.get('/aboutPage', (req, res) => {
     res.render('pages/aboutPage');
 });
 
-app.get('/commentsUploads.ejs', (req, res)=>{
+app.get('/commentsUploads', (req, res) => {
     res.render('pages/commentsUploads');
 });
 
-app.get('/contactPage.ejs', (req, res)=>{
+app.get('/contactPage', (req, res) => {
     res.render('pages/contactPage');
 });
 
-app.get('/galleryPage.ejs', (req, res)=>{
-    res.render('pages/galleryPage');
+app.get('/galleryPage', (req, res) => {   
+    // res.render('pages/galleryPage',
+    console.log(my_object);
+    db_handler.collection(COLLECTION_NAME).find({}).toArray((err, result) => {  
+      if (err) {   
+          console.log(err);
+       } else {
+res.render('/galleryPage', { 
+//   'allDetails': result 
+          })
+       }
+    })
+//   )
 });
 
-app.get('/mapPage.ejs', (req, res)=>{
+// app.get('/galleryPage', (req, res) => {
+//     res.render('pages/galleryPage', db_handler.collection(COLLECTION_NAME).find({}).toArray((err, result) => {
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             res.render('pages/allDetails', {
+//                 'allDetails': result
+//             }); 
+// );
+//             });
+//         )
+
+
+app.get('/mapPage', (req, res) => {
     res.render('pages/mapPage');
 });
+
+app.get('/createPage', (req, res) => {
+    console.log("user is here /createPage")
+    res.render('pages/createPage');
+});
+
+app.get('/allDetails', (req, res) => {
+    db_handler.collection(COLLECTION_NAME).find({}).toArray((err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('pages/allDetails', {
+                'allDetails': result
+            });
+        }
+    });
+});
+let my_object;
+app.post('/createPage', (req, res) => {
+    const form_data = req.body;
+    console.log(form_data);
+    const artName = form_data['artName'];
+    const address = form_data['address'];
+    const link = form_data['link'];
+
+    my_object = {
+        "artName": artName,
+        "address": address,
+        "link": link
+
+    }
+    db_handler.collection(COLLECTION_NAME).insertOne(my_object, (err, result) => {
+        if (err) {
+            console.log("Error: " + err);
+        }
+        else {
+            console.log("One Entry Added");
+            res.render('/allDetails');
+        }
+    });
+    // res.render('pages/allDetails')
+
+});
+
+
+
+// app.post('pages/allDetails', (req, res) => {
+//     // req.body contains form information
+//     const form_data = req.body;
+//     console.log(form_data);
+//     const artName = form_data['artName'];
+//     const address = form_data['address'];
+// 	const link = form_data['link'];
+
+
+// 	// const bags = parseInt(form_data['bags']);
+
+// 	const my_object = {
+//         "artName": artName,
+//         "address": address,
+//         "link": link
+//     }});
---
+++ public/scripts.js
@@ -0,0 +1,15 @@
+let info = document.getElementById('btn');
+let content = document.getElementById('content');
+
+info.addEventListener('click', showInfo);
+
+function showInfo(){
+content.style.display = "block";
+// console.log('Hey you');
+}
+
+// info.addEventListener('click', hideInfo);
+
+// function hideInfo(){
+//     // content.style.display ="none";
+// }
